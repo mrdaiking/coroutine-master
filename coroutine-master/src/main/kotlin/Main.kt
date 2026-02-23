a68ecc48d4e9e703e7e52fe89905040d9a8bc312
mrdaiking <daikynguyen999@gmail.com>
2026-02-24 07:39:16 +0900

    best practices

--- coroutine-master/src/main/kotlin/Main.kt
+++ coroutine-master/src/main/kotlin/Main.kt
@@ -1,8 +1,20 @@
 package org.example
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
+
+
+//suspend fun sayHello() {
+//    println("I'm saying hello (thread: ${Thread.currentThread().name})")
+//    delay(2000) // suspend 2s without blocking thread
+//    println("I'm done after 2s")
+//}
+//
+//fun main() = runBlocking { // runBlocking used to run suspend from main
+//    println("Main start (thread: ${Thread.currentThread().name})")
+//    sayHello()
+//    println("Main end")
+//}
 
 suspend fun fetchUserFromDb(): String {
     delay(1500)
@@ -20,11 +32,47 @@ suspend fun loadProfile(): String {
     return "$user\n$post"
 }
 
+suspend fun loadProfileConcurrently(): String = coroutineScope {
+    val userDeferred = async { fetchUserFromDb() }
+    val postDeferred = async { fetchPostsFromApi() }
+
+    val user = userDeferred.await()
+    val post = postDeferred.await()
+    "$user\n$post"
+}
+
 fun main() = runBlocking {
-    val time = measureTimeMillis {
-        println(loadProfile())
+//    val time = measureTimeMillis {
+////        println(loadProfile())
+//        println(loadProfileConcurrently())
+//    }
+//    println("Took $time ms")
+//    println("Start main")
+//
+//    val job = launch {
+//        println("Launch started")
+//        delay(1000)
+//        println("Launch finished")
+//    }
+//
+//    println("After launch")
+//
+//    job.join()
+//
+//    println("End main")
+    val job = launch {
+        repeat(5) { i ->
+            println("Working $i")
+            delay(500)
+        }
     }
-    println("Took $time ms")
+
+    delay(1200)
+    println("Cancel job")
+    job.cancel()
+    job.join()
+
+    println("Main finished")
 }
 
 
