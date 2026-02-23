671c8cca2841245be0cdf7d93f751bccbd2f721e
mrdaiking <daikynguyen999@gmail.com>
2026-02-23 17:51:00 +0900

    finish set up for kotlin 1.3.72

--- coroutine-master/src/main/kotlin/Main.kt
+++ coroutine-master/src/main/kotlin/Main.kt
@@ -1,24 +1,30 @@
 package org.example
 
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
+suspend fun fetchUserFromDb(): String {
+    delay(1500)
+    return "User: Cuong"
+}
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
+suspend fun fetchPostsFromApi(): String {
+    delay(2500)
+    return "3 posts from api"
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+suspend fun loadProfile(): String {
+    val user = fetchUserFromDb()
+    val post = fetchPostsFromApi()
+    return "$user\n$post"
+}
+
+fun main() = runBlocking {
+    val time = measureTimeMillis {
+        println(loadProfile())
     }
+    println("Took $time ms")
 }
 
-suspend fun loadProfile() = coroutineScope {
-    val user = async { fetchUser() }
-    val posts = async { fetchPosts() }
-}
+
